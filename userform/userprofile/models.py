81ab7b08225ee9e20814165fe19f9b3ec3fb1987
piyush.gupta <piyush.gupta@zeeve.io>
2022-06-18 17:10:32 +0530

    fixed models

--- userform/userprofile/models.py
+++ userform/userprofile/models.py
@@ -1,3 +1,22 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import UserManager
 
-# Create your models here.
+
+class user_reg(AbstractUser):
+    username = models.CharField(max_length=20, unique=True, blank=False)
+    mobile = models.CharField(max_length=10, blank=True, null=True, unique=True)
+    age = models.IntegerField(validators=[MinValueValidator(0)], null=True, blank=True)
+    gender = models.CharField(max_length=8, default='Male')
+    mobile = models.PositiveBigIntegerField(blank=False)
+
+    def __str__(self):
+        return self.username
+
+class Verification_Otp(models.Model):
+    user = models.ForeignKey(user_reg, on_delete=models.CASCADE, null=False)
+    expired = models.CharField(max_length=10, null=True)
+    pending = models.CharField(max_length=10, null=True)
+    created_at = models.DateTimeField()
+    attempts = models.IntegerField(default=0, blank=False)
